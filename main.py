648242b137a1e57e3006a17e24b2f94a93831451
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2023-12-24 20:50:42 +0100

    card lists for black jack

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-cards_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+
+def deal_the_cards():
+   cards_deck = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+   card = random.choice(cards_deck)
+   return card
+
+
 
 user_cards = []
 computer_cards = []
-
