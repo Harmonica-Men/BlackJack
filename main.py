432bd8ce264233d5bcc0cc8a1872ff13c9c5bcd2
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2023-12-25 11:38:58 +0100

    fix bug line 77 play_blackjack()

--- main.py
+++ main.py
@@ -40,42 +40,41 @@ def compare_player_vs_computer(userscore, computerscore):
     else:
         return "Sorry, You lost the game"
 
+# black jack game begins here
+def play_blackjack():
+    # create empty lists
+    user_cards = []
+    computer_cards = []
 
-# create empty lists
-user_cards = []
-computer_cards = []
-
-# deal the cards a put them in their lists
-for _var in range(2):  # run two loops
-    user_cards.append(deal_the_cards())
-    computer_cards.append(deal_the_cards())
-
-player_score = calculate_the_score(user_cards)
-computer_score = calculate_the_score(computer_cards)
-print(f" player: {user_cards}, current score {player_score}")
-print(f" computer top card of the deck: {computer_cards[0]}")
-
-if player_score == 0 or computer_score == 0 or player_score > 21:
-    over_and_out = True  # End the game
-else:
-    deal_again = input("Another card 'Y' or 'N' to pass")
-    if deal_again == "Yes":
+    # deal the cards a put them in their lists
+    for _var in range(2):  # run two loops
         user_cards.append(deal_the_cards())
-    else:
-        over_and_out = True
+        computer_cards.append(deal_the_cards())
 
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_the_cards())
+    player_score = calculate_the_score(user_cards)
     computer_score = calculate_the_score(computer_cards)
+    print(f" player: {user_cards}, current score {player_score}")
+    print(f" computer top card of the deck: {computer_cards[0]}")
 
-# print the score of player and computer
-print(f" last card: {user_cards}, final score: {player_score}")
-print(f" computer last card; {computer_cards}, final score : {computer_score}")
-print(compare_player_vs_computer(player_score, computer_score))
+    if player_score == 0 or computer_score == 0 or player_score > 21:
+        over_and_out = True  # End the game
+    else:
+        deal_again = input("Another card 'Y' or 'N' to pass")
+        if deal_again == "Yes":
+            user_cards.append(deal_the_cards())
+        else:
+            over_and_out = True
 
-play = input("Wanna play Black Jack? type 'y' or 'n' : ")
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_the_cards())
+        computer_score = calculate_the_score(computer_cards)
 
-if play == "y":
-        #  play_blackjack #
-else:
-    print("TY for playing Black Jack, See next time")
+    # print the score of player and computer
+    print(f" last card: {user_cards}, final score: {player_score}")
+    print(f" computer last card; {computer_cards}, final score : {computer_score}")
+    print(compare_player_vs_computer(player_score, computer_score))
+
+while input("Wanna play Black Jack? type 'y' or 'n' : ") == 'y':
+    play_blackjack()
+
+print("TY for playing Black Jack, See you next time")
